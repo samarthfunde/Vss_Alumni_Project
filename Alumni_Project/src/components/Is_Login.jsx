8ea4a5a52115d95918ffa946da42b6374233ccb9
Samarth Funde <122684505+samarthfunde@users.noreply.github.com>
2025-04-18 10:24:23 +0530

    Update Is_Login.jsx
    resolve  issue of responsivness

--- Alumni_Project/src/components/Is_Login.jsx
+++ Alumni_Project/src/components/Is_Login.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const IsLogin = () => {
-    // const location = useLocation();
     const navigate = useNavigate();
 
-  return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1 className="display-1"><span role="img" aria-label="sad emoji">😢</span></h1>
-      <h2>Please Sign In First</h2>
-      <p className="lead">Sorry, the page you are looking for does not exist.</p>
-      <button onClick={() => navigate(-1)} className="btn btn-primary d-block mx-auto">Head back</button>
-    </div>
-  )
-}
+    const handleLoginRedirect = () => {
+        navigate('/login', { state: { action: 'navtologin' } });
+    };
+
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+            {/* Using a simple emoji */}
+            <h1 className="display-1">
+                <span role="img" aria-label="sad emoji">😢</span>
+            </h1>
+            <h2>Please Sign In First</h2>
+            <p className="lead"></p>
+            <button onClick={handleLoginRedirect} className="btn btn-primary d-block mx-auto">
+                Login
+            </button>
+        </div>
+    );
+};
 
 export default IsLogin;
