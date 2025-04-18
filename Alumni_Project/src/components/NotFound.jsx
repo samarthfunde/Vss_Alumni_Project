6d572d54ab94caecc07dde3808f7c728d02b7592
Samarth Funde <122684505+samarthfunde@users.noreply.github.com>
2025-04-18 10:23:40 +0530

    Update NotFound.jsx
    resolve issue of responsivness

--- Alumni_Project/src/components/NotFound.jsx
+++ Alumni_Project/src/components/NotFound.jsx
@@ -9,7 +9,7 @@ const NotFound = () => {
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
       <h1 className="display-1"><span role="img" aria-label="sad emoji">😢</span></h1>
       <h2>Page Not Found</h2>
-      <p className="lead">Sorry, the page you are looking for does not exist.</p>
+     <br></br>
       <button onClick={() => navigate(-1)} className="btn btn-primary d-block mx-auto">Head back</button>
     </div>
   )
