3cd3eb624709230bbd95d7dc6188996e3cdc5b66
Samarth Funde <122684505+samarthfunde@users.noreply.github.com>
2025-05-12 15:55:52 +0530

    Update globalurl.js

--- Alumni_Project/src/utils/globalurl.js
+++ Alumni_Project/src/utils/globalurl.js
@@ -1,3 +1,3 @@
-export const baseUrl="https://vss-alumni-project-nfjn.vercel.app/";
+export const baseUrl="https://vss-alumni-backend.vercel.app/";
 
 // export const baseUrl="https://alumni-server-alpha.vercel.app/";
